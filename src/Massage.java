2be36d3bd972e5824375cc84b42be7ec77be8427
rinovai <pikokk@bk.ru>
2022-08-19 21:10:22 +0600

    Initial commit

---
+++ src/Massage.java
@@ -0,0 +1,26 @@
+public class Massage {
+    public static void main(String[] args) {
+
+
+        String One="Никита: Привет, друг!";
+        String Two="Олег: Привет!";
+        String Three="Никита: Как твои дела?";
+        String Four="Олег: Все хорошо, а у тебя?";
+        String Five="Никита: У меня тоже все хорошо!";
+        String Six="Олег: Ясно, пойдем завтра гулять?";
+        String Seven="Никита: Да, давай. Где встретимся и во сколько?";
+        String Eight="Олег: Давай в 14:00 возле моего дома";
+        String Nine= "Никита: Окей";
+ System.out.println(One+" "+Two+" "+Three+" "+Four+" "+Five+" "+Six+" "+Seven+" "+Eight+" "+Nine);
+
+ System.out.println(One);
+ System.out.println(Two);
+ 
+
+
+
+
+
+
+    }
+}
